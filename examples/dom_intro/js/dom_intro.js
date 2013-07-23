7e88566167fff6cf68b0586b373de047ed4a1761
Jamal Powell <jamalkpowell@gmail.com>
2013-07-23 16:37:36 -0400

    commit

--- examples/dom_intro/js/dom_intro.js
+++ examples/dom_intro/js/dom_intro.js
@@ -1,2 +1,14 @@
 // Traverse the DOM!
 
+function traverse_dom(search_term) {
+  // I want to traverse the dom and find all the divs
+  // elements
+  var elements = document.getElementsByTagName('div');
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].innerText === search_term) {
+      return elements[i];
+    }
+  }
+  return null;
+}
+
