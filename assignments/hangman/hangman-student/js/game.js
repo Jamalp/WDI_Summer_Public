0eef93622dad5d929d0b5930ee27da3d78485b5a
Jamal Powell <jamalkpowell@gmail.com>
2013-07-27 13:11:57 -0400

    working on checkletters

--- assignments/hangman/hangman-student/js/game.js
+++ assignments/hangman/hangman-student/js/game.js
@@ -1,14 +1,17 @@
 var word = {
+  alphabet = [];
   secretWord: "",
   wordList: ['ruby', 'rails', 'javascript', 'array', 'hash', 'underscore', 'sinatra', 'model', 'controller', 'view', 'devise', 'authentication', 'capybara', 'jasmine', 'cache', 'sublime', 'terminal', 'system', 'twitter', 'facebook', 'function', 'google', 'amazon', 'development', 'data', 'design', 'inheritance', 'prototype', 'gist', 'github', 'agile', 'fizzbuzz', 'route', 'gem', 'deployment', 'database'],
-
   // Selects a random word from the word list sets the secret word
-  setSecretWord: function(){},
+  setSecretWord: function(){
+    this.secretWord = _.shuffle(word.wordList)[0];
+  },
 
   // Takes an array of letters as input and returns an array of two items:
   // 1) A string with the parts of the secret word that have been guessed correctly and underscore for the parts that haven't
   // 2) An array of all the guessed letters that were not in the secret word
-  checkLetters: function(guessedLetters){}
+  checkLetters: function(guessedLetters){
+    if (_.contains(alphabet, guessedLetters)
 };
 
 var player = {
@@ -17,6 +20,7 @@ var player = {
 
   // Takes a new letter as input and updates the game
   makeGuess: function(letter){},
+
 
   // Check if the player has won and end the game if so
   checkWin: function(wordString){},
